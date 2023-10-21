ac261f08dd550e4faa303ade537dfb68ffc4dae9
Joshua Calafell <joshcalafell@gmail.com>
2023-10-21 12:41:34 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { technoSubgenres } from "./data";
+
+function TechnoSubgenreTable({ subgenres }) {
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>Subgenre</th>
+            <th>Description</th>
+            <th>Years of Popularity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {subgenres.map((subgenre, index) => (
+            <tr key={index}>
+              <td>{subgenre.subgenre}</td>
+              <td>{subgenre.description}</td>
+              <td>{subgenre.yearsOfPopularity}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <footer>
+        <p>Made with assistance from ChatGPT in under 20 minutes</p>
+      </footer>
+    </>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Techno Subgenres</h1>
+      <TechnoSubgenreTable subgenres={technoSubgenres} />
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,62 @@
+export const technoSubgenres = [
+  {
+    subgenre: "Detroit Techno",
+    description:
+      "Originating in Detroit, this genre is known for its soulful and melodic take on techno, with deep basslines and intricate melodies.",
+    yearsOfPopularity: "Mid-1980s to Present",
+  },
+  {
+    subgenre: "Minimal Techno",
+    description:
+      "Characterized by stripped-down, minimalistic arrangements, and a focus on subtle, repetitive patterns, this genre is often associated with a sense of hypnotism and simplicity.",
+    yearsOfPopularity: "Late 1990s to Present",
+  },
+  {
+    subgenre: "Industrial Techno",
+    description:
+      "With heavy use of distortion and darker themes, industrial techno has a raw and abrasive sound often used in underground and industrial settings.",
+    yearsOfPopularity: "Late 1980s to Present",
+  },
+  {
+    subgenre: "Hard Techno",
+    description:
+      "Known for its aggressive beats and high energy, hard techno is associated with powerful kick drums and is popular in the techno rave scene.",
+    yearsOfPopularity: "Mid-1990s to Present",
+  },
+  {
+    subgenre: "Acid Techno",
+    description:
+      "Characterized by the use of the Roland TB-303 synthesizer, acid techno has a distinctive, squelchy sound and is known for its infectious grooves.",
+    yearsOfPopularity: "Late 1980s to Present",
+  },
+  {
+    subgenre: "Trance Techno",
+    description:
+      "A fusion of trance and techno, this subgenre is recognized for its euphoric melodies, uplifting chords, and energetic rhythms.",
+    yearsOfPopularity: "1990s to Present",
+  },
+  {
+    subgenre: "Progressive Techno",
+    description:
+      "This subgenre incorporates progressive house elements into techno, offering a more gradual and melodic progression within tracks.",
+    yearsOfPopularity: "1990s to Present",
+  },
+  {
+    subgenre: "Electro Techno",
+    description:
+      "Electro techno combines elements of electro and techno, featuring crisp, synthetic beats and often a futuristic, robotic sound.",
+    yearsOfPopularity: "Late 1990s to Present",
+  },
+  {
+    subgenre: "Dub Techno",
+    description:
+      "Dub techno places emphasis on echo and reverb effects, creating a spacious, hypnotic sound with roots in Jamaican dub music.",
+    yearsOfPopularity: "1990s to Present",
+  },
+  {
+    subgenre: "Ambient Techno",
+    description:
+      "Focusing on atmospheric and ambient qualities, this subgenre blends techno beats with lush, ethereal textures for a more relaxed and introspective experience.",
+    yearsOfPopularity: "1990s to Present",
+  },
+];
